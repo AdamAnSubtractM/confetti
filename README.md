913e0caab7647aef29db95fda618d5aa3672fd18
Adam Knee <adam.l.knee@gmail.com>
2024-03-20 23:39:41 -0400

    Add better docs. Create Github Action to punlish JSR package.

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # confetti
+
 A package that allows you to throw confetti on the page.
+
+## Throwing confetti on the page
+
+You can throw confetting on the page by writing the following:
+
+```ts
+import { throwConfetti } from '@adam/confetti'
+
+const button = document.querySelector('#confetti-button')
+
+button.addEventListenter('click', throwConfetti)
+```
+
+## Passing optional options
+
+The throwConfetti function can also take an optional options object to control the behavior of the confetti and canvas.
+
+- canvasId?: string
+  - Queries for a custom canvasId if passed. If not found, it uses this id to generate a canvas.
+- customStyles?: CSSStyleDeclaration
+  - Allows an object of custom styles to be applied to the canvas.
+- particleCount?: number
+  - Allows for a custom number of particles to be generated on the canvas.
+- secondsUntilDeletion?: number
+  - Allows for the canvas to be auto deleted after the number of seconds defined here has passed.
+- selectorToAppend?: string
+  - Allows a custom selector to be defined for where the canvas is appended.
+
+```ts
+import { throwConfetti } from '@adam/confetti'
+
+const button = document.querySelector('#confetti-button')
+
+button.addEventListenter('click', () => {
+  throwConfetti({
+    canvasId: 'custom-canvas-id',
+    customStyles: { width: '500px', height: '500px' },
+    particleCount: 300,
+    secondsUntilDeletion: 8,
+    selectorToAppend: 'main',
+  })
+})
+```
--- src/confetti/confetti.ts
+++ src/confetti/confetti.ts
@@ -1,4 +1,8 @@
 // confetti.ts
+
+/**
+ * The default canvas styling. Import this to extend the canvas styles or override it all together.
+ */
 export const defaultCanvasStyles = {
   position: 'absolute',
   zIndex: -1,
@@ -6,6 +10,9 @@ export const defaultCanvasStyles = {
   pointerEvents: 'none',
 }
 
+/**
+ * The confetti particle config
+ */
 type ConfettiParticleConfig = {
   canvas: HTMLCanvasElement
   ctx: CanvasRenderingContext2D
@@ -18,7 +25,7 @@ type ConfettiParticleConfig = {
 
 /**
  * Creates a confetti particle
- * @param config
+ * @param config - The required configuration to create a confetti particle.
  * @returns A confetti particle
  */
 const createConfettiParticle = function ({ canvas, ctx, color, size, velocity, x, y }: ConfettiParticleConfig) {
@@ -45,6 +52,9 @@ const createConfettiParticle = function ({ canvas, ctx, color, size, velocity, x
   }
 }
 
+/**
+ * The confetti particles settings.
+ */
 type ConfettiParticlesSettings = Omit<ConfettiParticleConfig, 'color' | 'size' | 'velocity' | 'x' | 'y'> & {
   count: number
 }
@@ -52,7 +62,7 @@ type ConfettiParticlesSettings = Omit<ConfettiParticleConfig, 'color' | 'size' |
 /**
  *
  * @param settings The confetti particles settings
- * @returns The confetti particles config
+ * @returns The an Array of confetti particle instances.
  */
 const createConfettiParticles = function ({ canvas, ctx, count }: ConfettiParticlesSettings) {
   const particles = []
@@ -71,13 +81,16 @@ const createConfettiParticles = function ({ canvas, ctx, count }: ConfettiPartic
   return particles
 }
 
+/**
+ * The animate confetti particles settings.
+ */
 type AnimateConfettiParticlesSettings = Omit<ConfettiParticleConfig, 'color' | 'size' | 'velocity' | 'x' | 'y'> & {
   particles: ReturnType<typeof createConfettiParticle>[]
 }
 
 /**
- * Animates the confetti particles on the canvas
- * @param settings The animation settings for the confetti
+ * Animates the confetti particles on the canvas.
+ * @param settings The animation settings for the confetti.
  */
 const animateConfettiParticles = function ({ canvas, ctx, particles }: AnimateConfettiParticlesSettings) {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
@@ -89,14 +102,51 @@ const animateConfettiParticles = function ({ canvas, ctx, particles }: AnimateCo
 }
 
 /**
- * Deletes the canvas that was used to generate the confetti
- * @param canvas The canvas that has the confetti
+ * Deletes the canvas that was used to generate the confetti.
+ * @param canvas The canvas that has the confetti.
  */
 const deleteCanvas = function (canvas: HTMLCanvasElement) {
   canvas.remove()
 }
 
-type ThrowConfettiSettings = {
+/**
+ * The canvas options that can be passed to the setupCanvas function.
+ */
+type CanvasOptions = Omit<ThrowConfettiOptions, 'canvasId' | 'customStyles' | 'selectorToAppend'>
+
+/**
+ * Sets up the canvas by calling the createConfettiParticles and animateConfettiParticles functions.
+ * If the secondsUntilDeletion option is passed, it automatically deletes the Canvas when completing
+ * the timeout.
+ * @param canvas The HTMLCanvasElement that was created initially by the throwConfetti function.
+ * @param options
+ */
+function setupCanvas(canvas: HTMLCanvasElement, options?: CanvasOptions) {
+  const ctx = canvas?.getContext('2d')
+  if (!ctx) {
+    throw new Error('Unable to get canvas context.')
+  }
+
+  const particles = createConfettiParticles({
+    canvas,
+    ctx,
+    count: options?.particleCount || 100,
+  })
+
+  animateConfettiParticles({ canvas, ctx, particles })
+
+  if (options?.secondsUntilDeletion && typeof options.secondsUntilDeletion === 'number') {
+    setTimeout(() => deleteCanvas(canvas), options.secondsUntilDeletion * 1000)
+  }
+}
+
+/**
+ * The options that can be passed to the throwConfetti function.
+ * This allows you to define a custom canvas id, custom styles for the canvas,
+ * a custom particle count, the number of seconds until the canvas is deleted
+ * from the DOM, and a custom selector to append the canvas to.
+ */
+type ThrowConfettiOptions = {
   canvasId?: string
   customStyles?: CSSStyleDeclaration
   particleCount?: number
@@ -105,33 +155,37 @@ type ThrowConfettiSettings = {
 }
 
 /**
- * Generates a canvas that has confetti in it
- * @param settings Optional settings object to configure how the confetti behaves
+ * Generates a canvas that has confetti in it.
+ * @param options Optional settings object to configure how the confetti behaves.
+ *
+ * ```ts
+ * throwConfetti()
+ * ```
+ *
+ * ```ts
+ * throwConfetti({
+ *  canvasId: "custom-canvas-id",
+ *  customStyles: { width: "500px", height: "500px" },
+ *  particleCount: 300,
+ *  secondsUntilDeletion: 8,
+ *  selectorToAppend: "main",
+ * })
+ * ```
  */
-export const throwConfetti = function (settings?: ThrowConfettiSettings) {
-  const canvasId = settings?.canvasId || 'confetti-canvas'
+export const throwConfetti = function (options?: ThrowConfettiOptions) {
+  const canvasId = options?.canvasId || 'confetti-canvas'
   let canvas: HTMLCanvasElement | null = document.querySelector(`#${canvasId}`)
+
   if (!canvas) {
     canvas = document.createElement('canvas')
     canvas.id = canvasId
-    document.querySelector(settings?.selectorToAppend || 'body')?.appendChild(canvas)
+    document.querySelector(options?.selectorToAppend || 'body')?.appendChild(canvas)
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
-    Object.assign(canvas.style, settings?.customStyles || defaultCanvasStyles)
-  }
-  const ctx = canvas.getContext('2d')
+    Object.assign(canvas.style, options?.customStyles || {})
 
-  if (canvas && ctx) {
-    const particles = createConfettiParticles({
-      canvas,
-      ctx,
-      count: settings?.particleCount || 100,
-    })
-    animateConfettiParticles({ canvas, ctx, particles })
-    if (settings?.secondsUntilDeletion && typeof settings.secondsUntilDeletion === 'number') {
-      setTimeout(() => deleteCanvas(canvas), settings.secondsUntilDeletion * 1000)
-    }
+    setupCanvas(canvas, options)
   } else {
-    throw new Error(`Unable to throw confetti. The canvas ctx was not found.`)
+    setupCanvas(canvas, options)
   }
 }
